5a5679503264d60f63543ed88c27344d6b02bc09
Jonas Hertzman <jonas.hertzman@ifsworld.com>
2019-04-01 18:30:13 +0200

    Extended datagrid to override keyboard behavior

---
+++ FileSearch3/ExtendedDataGrid.cs
@@ -0,0 +1,24 @@
+﻿using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace FileSearch
+{
+	class ExtendedDataGrid : DataGrid
+	{
+
+		#region Overrides
+
+		protected override void OnKeyDown(KeyEventArgs e)
+		{
+			if (e.Key == Key.Left || e.Key == Key.Right)
+			{
+				return;
+			}
+
+			base.OnKeyDown(e);
+		}
+
+		#endregion
+
+	}
+}
