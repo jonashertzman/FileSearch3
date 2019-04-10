c95acd2164ac3e8685b64df435867a89c0c15bad
Jonas Hertzman <jonas.hertzman@ifsworld.com>
2019-04-10 10:29:23 +0200

    Long lines crash the preview

--- FileSearch3/TextUtils.cs
+++ FileSearch3/TextUtils.cs
@@ -61,11 +61,6 @@ namespace FileSearch
 				return null;
 			}
 
-			if (text.Length > 65535)
-			{
-				text = "[Line is too long to show in preview]";
-			}
-
 			FontData fontData = GetFontData(typeface, fontSize);
 
 			ushort[] glyphIndexes = new ushort[text.Length];
