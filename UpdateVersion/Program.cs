70fd1d5c493ed6cb154e15209c24da60dd8a6586
Jonas Hertzman <jonas.hertzman@gmail.com>
2021-01-04 18:59:06 +0100

    Publish settings

--- UpdateVersion/Program.cs
+++ UpdateVersion/Program.cs
@@ -5,14 +5,14 @@ namespace UpdateVersion
 {
 	class Program
 	{
-		static void Main(string[] args)
+		static void Main()
 		{
 			DateTime buildDate = DateTime.Now;
 			string buildNumber = $"{buildDate:yy}{buildDate.DayOfYear:D3}";
 
 			Console.WriteLine($"Updating version to {buildNumber}");
 
-			File.WriteAllText(@"..\..\..\docs\download\version.txt", buildNumber);
+			File.WriteAllText(@"..\docs\download\version.txt", buildNumber);
 		}
 	}
 }
