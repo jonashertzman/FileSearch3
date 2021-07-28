c2189880dc413898ac511b06531319057f98f7e3
Jonas Hertzman <jonas.hertzman@gmail.com>
2021-07-28 17:09:23 +0200

    Create download archive when updating version number

--- UpdateVersion/Program.cs
+++ UpdateVersion/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.IO;
+using System.IO.Compression;
 
 namespace UpdateVersion
 {
@@ -13,6 +14,15 @@ namespace UpdateVersion
 			Console.WriteLine($"Updating version to {buildNumber}");
 
 			File.WriteAllText(@"..\docs\download\version.txt", buildNumber);
+
+
+			Console.WriteLine($"Updating download");
+
+			File.Delete(@"..\docs\download\FileSearch3.zip");
+
+			using ZipArchive download = ZipFile.Open(@"..\docs\download\FileSearch3.zip", ZipArchiveMode.Create);
+			download.CreateEntryFromFile(@".\bin\Publish\FileSearch.exe", "FileSearch.exe");
+			download.CreateEntryFromFile(@"..\LICENSE", "LICENSE");
 		}
 	}
 }
