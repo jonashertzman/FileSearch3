abbf74eae753fcd6c3923b53c009642155e8e608
Jonas Hertzman <jonas.hertzman@gmail.com>
2021-08-17 22:17:49 +0200

    Add possibility to paste multiple search phrases

--- FileSearch3/MainWindow.xaml.cs
+++ FileSearch3/MainWindow.xaml.cs
@@ -789,6 +789,20 @@ namespace FileSearch
 			e.Handled = true;
 		}
 
+		private void DataGridSearchPhrases_KeyDown(object sender, KeyEventArgs e)
+		{
+			if (e.Key == Key.V && (Keyboard.Modifiers & ModifierKeys.Control) == ModifierKeys.Control)
+			{
+				if (Clipboard.ContainsText())
+				{
+					foreach (string line in ((string)Clipboard.GetData(DataFormats.Text)).Split(new[] { "\r\n", "\r", "\n" }, StringSplitOptions.None))
+					{
+						ViewModel.ActiveSearchInstance.SearchPhrases.Add(new TextAttribute(line));
+					}
+				}
+			}
+		}
+
 		#endregion
 
 		#region Commands
