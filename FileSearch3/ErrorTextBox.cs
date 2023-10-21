337b5727bd9aa7a96cf52cf8e7082bd472738cde
Jonas Hertzman <jonas.hertzman@gmail.com>
2023-10-21 16:12:14 +0200

    Colors

---
+++ FileSearch3/ErrorTextBox.cs
@@ -0,0 +1,22 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace FileSearch;
+
+public class ErrorTextBox : TextBox
+{
+	#region Dependency Properties
+
+	public static readonly DependencyProperty ErrorProperty = DependencyProperty.Register("Error", typeof(bool), typeof(ErrorTextBox));
+
+	public bool Error
+	{
+		get { return (bool)GetValue(ErrorProperty); }
+		set
+		{
+			SetValue(ErrorProperty, value);
+		}
+	}
+
+	#endregion
+}
