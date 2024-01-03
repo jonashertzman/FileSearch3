a5c742bdf5d349119de996a2d366f6ff03675251
Jonas Hertzman <jonas.hertzman@gmail.com>
2024-01-03 13:33:13 +0100

    Merge settings on startup

--- FileSearch3/Windows/MainWindow.xaml.cs
+++ FileSearch3/Windows/MainWindow.xaml.cs
@@ -66,7 +66,7 @@ public partial class MainWindow : Window
 
 	private void LoadSettings()
 	{
-		AppSettings.ReadSettingsFromDisk();
+		AppSettings.LoadSettings();
 
 		this.Left = AppSettings.PositionLeft;
 		this.Top = AppSettings.PositionTop;
