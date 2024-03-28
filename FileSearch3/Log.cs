2ab5429cb8c948fb61d57086a2237d063dba95f5
Jonas Hertzman <jonas.hertzman@gmail.com>
2024-03-28 15:39:25 +0100

    Log exception type

--- FileSearch3/Log.cs
+++ FileSearch3/Log.cs
@@ -10,7 +10,7 @@ internal static class Log
 
 	public static void LogUnhandledException(Exception exception, string source)
 	{
-		string errorText = $"{DateTime.UtcNow} - {source}\n{exception.Message}\n{exception.StackTrace}\n\n";
+		string errorText = $"{DateTime.UtcNow} - {source}\nException:  {exception.GetType().Name}\nMessage:    {exception.Message}\n{exception.StackTrace}\n\n";
 
 		Directory.CreateDirectory(Path.GetDirectoryName(AppSettings.LogPath));
 		File.AppendAllText(AppSettings.LogPath, errorText);
